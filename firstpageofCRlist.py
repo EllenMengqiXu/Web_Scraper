9eeb28c0bc0bbb7f2f9323ae6fa4e42bd623deb1
EllenMengqiXu <40548240+EllenMengqiXu@users.noreply.github.com>
2018-07-23 16:58:51 -0400

    Add files via upload

---
+++ firstpageofCRlist.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import ChineserestaurantItem
+
+class FirstpageSpider(scrapy.Spider):
+    name = 'firstpage'
+    allowed_domains = ['cgjss.com']
+    start_urls = ['http://www.cgjss.com/ssql.php?1=1&page=1/']
+
+    def parse(self, response):
+        rows = response.css('.n')
+        for R in rows:
+            title = R.css(".z1::text").extract()
+            info = R.css(".k::text").extract()
+            item = ChineserestaurantItem()
+            item['title'] = title
+            item['info'] = info
+            yield item
